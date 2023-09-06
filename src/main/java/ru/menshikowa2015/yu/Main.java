20bb2707feef26a6e0e08835cc42b4ac9e59b70d
Julia <87570024+yuliya1254@users.noreply.github.com>
2023-09-06 23:13:29 +0300

    Update Main.java

--- src/main/java/ru/menshikowa2015/yu/Main.java
+++ src/main/java/ru/menshikowa2015/yu/Main.java
@@ -2,6 +2,6 @@ package ru.menshikowa2015.yu;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!!!!");
     }
 }
